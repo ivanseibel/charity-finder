4b5eb39f6a06df3638eb405423d7c4d79424ef08
Ivan Seibel <me@ivanseibel.dev>
2021-02-01 07:38:20 -0300

    chore: show modal presentation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useEffect } from 'react';
 
 import * as SC from './styles';
+import { ReactComponent as ReactLogo } from '../../public/assets/images/charity-finder-icon-blue.svg';
+import { useModal } from '../../hooks/useModal';
 
 const Home: React.FC = () => {
+  const { handleModal } = useModal();
+
+  const message = (
+    <>
+      <h2>Welcome to Charity Finder!</h2>
+      <p>
+        This site is an independent project that uses the public database made
+        available by{' '}
+        <a
+          style={{ textDecoration: 'none' }}
+          href="https://globalgiving.org"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GlobalGiving.org
+        </a>
+        . Through this website you can find institutions around the world, learn
+        about their projects and make a contribution to those who most need our
+        help.
+      </p>
+    </>
+  );
+
+  useEffect(() => {
+    handleModal(message);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <SC.MainHeader>
-        <SC.ContentContainer>
-          <p>Home</p>
-        </SC.ContentContainer>
+        <SC.HeaderContainer>
+          <div className="container-logo">
+            <ReactLogo />
+            <div className="container-logo-texts">
+              <h1 className="logo-title">CharityFinder</h1>
+              <span className="logo-description">Search and do good</span>
+            </div>
+          </div>
+        </SC.HeaderContainer>
       </SC.MainHeader>
       <SC.MainBody>
         <SC.ContentContainer>
